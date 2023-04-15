287fefd324992da280d34a318cfb8f161c026675
Mona Lisa <mona@Lisa.com>
2023-04-15 21:27:48 +0200

    "opdracht 12"

--- site/index.php
+++ site/index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta hhtp-equiv="X-UA-Compatible" content= "IE=edge">
+    <meta name="viewport" content="width=device-width , intial-scale= 1.0">
+    <title> Mijn eerste project</title>
+    
+</head>
+<body>
 <?php
  echo "Im a big fan of the Simpsons";
 ?>
+</body>
+</html>
