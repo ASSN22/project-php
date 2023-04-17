8498a784aad20124704e58e44055ef4309cfb7d3
Mona Lisa <mona@Lisa.com>
2023-04-17 13:03:43 +0200

    "opdracht 18"

--- site/index.php
+++ site/index.php
@@ -14,32 +14,47 @@ $voornaam = "Nour ";
 $achternaam = "Al-Shammari ";
 $leeftijd = "23 ";
 
-echo $voornaam, $achternaam, $leeftijd;
-?> <br>
+echo $voornaam, $achternaam, $leeftijd, nl2br("\n");
 
-<?php
-$bonusGetallen = [5,6,19,32,56,99];
+$geboortejaar = "1999 ";
+
+ if($geboortejaar > 1997 && $geboortejaar < 2012){
+    echo "Jij behoort tot de generatie Z, (Zoomer) ", nl2br("\n");
+}
+
+$bonusGetallen = [5,6,19,32,56,99]; 
 $mijnGegevens = ["Bart", 78, TRUE, 3.4];
 $meervoudigeArray = [[1,2,3], [4,5,6]];
+
+print_r($bonusGetallen); echo  nl2br("\n");
+
 foreach ($bonusGetallen as $item ){
     echo $item . "\n";
 }
+echo nl2br("\n");
 foreach ($mijnGegevens as $item ){
     echo $item . "\n";
 }
+echo nl2br("\n");
 foreach ($meervoudigeArray as $subArray ){
     foreach ($subArray as $item) {
     echo $item . "\n";}
 }
-?>
-<br>
+echo nl2br("\n");
 
-<?php
-$geboortejaar = "1999 ";
+$arrayLetters = [ "a", "b", "c", "d"];
+$arrayGetallen = [ 24, 56, 78, 90 ];
 
- if($geboortejaar > 1997 && $geboortejaar < 2012){
-    echo "Jij behoort tot de generatie Z, (Zoomer) ";
-}
+echo $arrayLetters[0], $arrayGetallen[1], "\n", $arrayLetters[2], $arrayGetallen[3], nl2br("\n");
+
+$mijnGegevens = [ 
+    "voornaam" => "Nour",
+    "achternaam" => "Al-Shammari",
+    "leeftijd" => 23
+];
+
+echo $mijnGegevens["voornaam"], "\n", $mijnGegevens["achternaam"], "\n", $mijnGegevens["leeftijd"], "\n", nl2br("\n");
+
 ?>
 </body>
 </html>
